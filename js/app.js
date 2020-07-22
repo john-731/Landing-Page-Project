23ca2f88e11503ab667df3f892b564008421954b
john-731 <59319231+john-731@users.noreply.github.com>
2020-07-22 13:53:58 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 //__________Defining global variables________________________________________________________________________
 const sections = document.querySelectorAll('section');
 const list = document.getElementById('navbar__list');
-const links = document.querySelectorAll('ul li a');
+const links = document.querySelectorAll('.navbar__menu .menu__link__active');
 
 
 //__________Building the nav_________________________________________________________________________________
 function navBuild(sections) {
     
-    for (const section of sections) {
-        const sectionID = section.id;
-        const sectionDN = section.dataset.nav;              
-        list.innerHTML += `<li><a href="#${sectionID}">${sectionDN}</a></li>`;
+    for (const section of sections) {          
+        list.innerHTML += `<li class="menu__link" id="${section.id}z"><a href="#${section.id}">${section.dataset.nav}</a></li>`;
               };
 };
         
@@ -23,9 +21,12 @@ window.addEventListener('scroll', function() {
 
         if(position.top >= 0 && position.bottom < window.innerHeight) {
 	        section.classList.add('your-active-class') ;
+            //document.getElementById('section2z').classList.add('menu__link__active');
+            /*linkID = */document.getElementById(`${section.id}z`).classList.add('menu__link__active');
+            //linkID.classList.add('menu__link__active');
             } else {
                 section.classList.remove('your-active-class');
-                links.classList.add('your-active-class');
+                document.getElementById(`${section.id}z`).classList.remove('menu__link__active');
         };
     };
 });
